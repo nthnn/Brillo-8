36d349e08afed34eae6f6a40a9c5a83c3de81edb
nthnn <nathanneisip@gmail.com>
2023-09-16 03:15:34 +0800

    Assembler main file.

--- assembler/src/assembler.ts
+++ assembler/src/assembler.ts
@@ -1,5 +1,25 @@
+import colors from '@colors/colors';
+import fs from 'fs';
+import process from 'process';
+
 function main(): void {
-    console.log("Brillo-8 Assembler");
+    colors.enable();
+
+    console.log("┌────────────────────────────────────────────────────────┐".cyan);
+    console.log("│".cyan + "                   Brillo-8 Assembler".blue.bold + "                   │".cyan);
+    console.log("│".cyan + "                    v0.0.1".yellow + " (Release)  ".brightYellow.italic + "                  │".cyan);
+    console.log("├────────────────────────────────────────────────────────┤".cyan);
+
+    let fileName: string = process.argv[2];
+    if(fileName == undefined) {
+        console.log("│".cyan + "  Usage:                                                " + "│".cyan);
+        console.log("│    ".cyan + "b8asm <filename>".brightYellow + "   Convert assembly file to binary. ".italic + "│".cyan);
+    }
+    else if(!fs.existsSync(fileName))
+        console.log("│  ".cyan + "Input file not found.".red + "                                 │".cyan);
+
+    console.log("└────────────────────────────────────────────────────────┘".cyan);
+    console.log();
 }
 
 main();
