b34cdd0edc57f01060fbc81ed4e09f5a8c20117f
nthnn <nathanneisip@gmail.com>
2023-09-16 04:16:55 +0800

    Unfinished OpCode list.

---
+++ assembler/src/opcodes.ts
@@ -0,0 +1,10 @@
+export const Brillo8Opcodes: Map<string, string> =
+    new Map<string, string>([
+    ["PUSH",    "00000000"],
+    ["POP",     "00000001"],
+    ["ADD",     "00000010"],
+    ["SUB",     "00000011"],
+    ["MUL",     "00000100"],
+    ["DIV",     "00000101"],
+    ["REM",     "00000110"],
+]);
