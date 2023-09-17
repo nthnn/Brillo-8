3f0fc7641051c4eafcd92c925ec886455984f9ac
nthnn <nathanneisip@gmail.com>
2023-09-18 02:59:03 +0800

    Function and operational assembly language for Brillo-8.

--- assembler/src/assembler.ts
+++ assembler/src/assembler.ts
@@ -4,13 +4,27 @@ import os from 'node:os';
 import process from 'node:process';
 import Brillo8Tokenizer from './tokenizer';
 import { Brillo8IOUtil } from './util';
+import Brillo8Transpiler from './transpiler';
 
 function processFile(fileName: string): void {
-    let tokenizer: Brillo8Tokenizer = new Brillo8Tokenizer(
-        Brillo8IOUtil.readFile(fileName)
-    );
+    let contents: string = Brillo8IOUtil.readFile(fileName);
+    console.log("  ‣ ".green + "Reading input file.");
 
+    let tokenizer: Brillo8Tokenizer = new Brillo8Tokenizer(contents);
+    console.log("  ‣ ".green + "Starting lexical parsing...");
     tokenizer.scan();
+    console.log("  ‣ ".green + "Lexical analysis done!");
+
+    let transpiler: Brillo8Transpiler = new Brillo8Transpiler(tokenizer.tokens);
+    console.log("  ‣ ".green + "Transpiling assembly file...");
+
+    let transpiled: string = transpiler.transpile();
+    console.log("  ‣ ".green + "Done transpilation.");
+
+    let outputFilename: string = fileName.substring(0, fileName.length - 4) + ".bin";
+    console.log("  ‣ ".green + "Writing output to file: " + outputFilename.green);
+
+    Brillo8IOUtil.writeFile(outputFilename, transpiled);
 }
 
 function main(): void {
@@ -20,19 +34,18 @@ function main(): void {
     console.log("┌────────────────────────────────────────────────────────┐".cyan);
     console.log("│".cyan + "                   Brillo-8 Assembler".blue.bold + "                   │".cyan);
     console.log("│".cyan + "                    v0.0.1".yellow + " (Release)  ".brightYellow.italic + "                  │".cyan);
-    console.log("├────────────────────────────────────────────────────────┤".cyan);
 
     let fileName: string = process.argv[2];
     if(fileName == undefined) {
+        console.log("├────────────────────────────────────────────────────────┤".cyan);
         console.log("│".cyan + "  Usage:                                                " + "│".cyan);
         console.log("│    ".cyan + "b8asm <filename>".brightYellow + "   Convert assembly file to binary. ".italic + "│".cyan);
     }
-    else if(!fs.existsSync(fileName))
+    else if(!fs.existsSync(fileName)) {
+        console.log("├────────────────────────────────────────────────────────┤".cyan);
         console.log("│  ".cyan + "Input file not found.".red + "                                 │".cyan);
-    else console.log("│  ".cyan + "Compiling input file...                               " + "│".cyan);
-
-    console.log("└────────────────────────────────────────────────────────┘".cyan);
-    console.log();
+    }
+    console.log("└────────────────────────────────────────────────────────┘\n".cyan);
 
     if(fileName != undefined && fs.existsSync(fileName)) {
         console.log("  ‣ ".green + "Compiling " + fileName.green);
