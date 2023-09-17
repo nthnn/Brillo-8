09419fd131377f7f81410ae03a34499bef4e4a00
nthnn <nathanneisip@gmail.com>
2023-09-17 23:21:01 +0800

    Improved tokenizer utility class files.

--- assembler/src/util.ts
+++ assembler/src/util.ts
@@ -1,11 +1,33 @@
 import fs from 'node:fs';
+import { Brillo8Opcodes } from './opcodes';
 
-export default class IOUtil {
-    static readFile(fileName: string): string {
+export class Brillo8IOUtil {
+    public static readFile(fileName: string): string {
         return fs.readFileSync(fileName, 'utf-8');
     }
 
-    static writeFile(fileName: string, contents: string): void {
+    public static writeFile(fileName: string, contents: string): void {
         fs.writeFileSync(fileName, contents);
     }
+}
+
+export function opcodeBinary(opcode: string): string {
+    return Brillo8Opcodes.get(opcode) as string;
+}
+
+export function toBinary(num: string): string {
+    let dec: number = parseInt(num);
+    if(dec == 0)
+        return "00000000";
+
+    let binResult: string = "";
+    while(dec > 0) {
+        binResult = (dec % 2) + binResult;
+        dec = Math.floor(dec / 2);
+    }
+
+    while(binResult.length < 8)
+        binResult = "0" + binResult;
+
+    return binResult;
 }
