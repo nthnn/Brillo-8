41f9fa6d0e041366029d576c0743be26eca13f7b
nthnn <nathanneisip@gmail.com>
2023-09-27 23:18:57 +0800

    Underdevelopment notice.

--- docs/src/scripts/pages/docs.tsx
+++ docs/src/scripts/pages/docs.tsx
@@ -4,7 +4,12 @@ import NavigationBar from "../components/navigationbar";
 export default class Docs extends React.Component {
     render() {
         return (
-            <NavigationBar currentPage="docs" />
+            <>
+                <NavigationBar currentPage="docs" />
+                <br/><br/><br/>
+
+                <h1 align="center">Under development.</h1>
+            </>
         );
     }
 }
